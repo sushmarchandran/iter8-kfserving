35c0a5cd6072e83c5074c93ae509975efe9577d6
Srinivasan Parthasarathy <spartha@us.ibm.com>
2020-10-10 17:44:32 -0400

    source

--- bin/install-everything.sh
+++ bin/install-everything.sh
@@ -8,6 +8,6 @@ git clone https://github.com/kubeflow/kfserving.git
 
 cd kfserving
 
-./hack/quick_install.sh
+source ./hack/quick_install.sh
 
 kubectl apply --filename https://github.com/knative/serving/releases/download/v0.18.0/monitoring-metrics-prometheus.yaml

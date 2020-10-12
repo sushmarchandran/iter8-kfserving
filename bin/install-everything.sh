a4b3d4b4c6649e259fb8a9282be34271a0ecaa88
Srinivasan Parthasarathy <spartha@us.ibm.com>
2020-10-12 15:24:40 -0400

    metrics configmap

--- bin/install-everything.sh
+++ bin/install-everything.sh
@@ -13,3 +13,9 @@ cd kfserving
 kubectl create ns knative-monitoring
 
 kubectl apply --filename https://github.com/knative/serving/releases/download/v0.18.0/monitoring-metrics-prometheus.yaml
+
+cd ../../
+
+kubectl create ns iter8-kfserving
+
+kubectl apply -k kustomize/metrics -n iter8-kfserving

5ad8da623253bca7eb44680feb58b6d73352b0aa
Srinivasan Parthasarathy <spartha@us.ibm.com>
2021-01-04 10:11:45 -0500

    I72fix (#74)
    * fixes inferenceservicesetup.sh so that ns already exists error is ignored
    
    * new line

--- samples/quickstart/inferenceservicesetup.sh
+++ samples/quickstart/inferenceservicesetup.sh
@@ -4,6 +4,6 @@ set -e
 
 # InferenceService setup for quickstart and e2e tests
 
-kubectl create ns kfserving-test
+kubectl create ns kfserving-test || true
 kubectl apply -f https://raw.githubusercontent.com/iter8-tools/iter8-kfserving/main/samples/common/sklearn-iris.yaml -n kfserving-test
 kubectl wait --for condition=ready --timeout=180s inferenceservice/sklearn-iris -n kfserving-test
